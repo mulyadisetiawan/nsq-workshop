fd2f66338c0188d586c111909b140abcd30e232b
rhomacahyanti <rhoma.cahyanti@tokopedia.com>
2020-11-06 18:13:37 +0700

    rhoma-nsq-workshop

--- api/ovo/ovo.go
+++ api/ovo/ovo.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+
+	"github.com/sharring_session/nsq/nsq-workshop/publisher"
 )
 
 type Response struct {
@@ -12,7 +14,28 @@ type Response struct {
 	Error string `json:"error"`
 }
 
+type RequestGiveBenefit struct {
+	UserID int
+}
+
 func GiveBenefit(userID int) error {
+
+	req := RequestGiveBenefit{
+		UserID: userID,
+	}
+
+	payload, err := json.Marshal(req)
+	if err != nil {
+		return err
+	}
+
+	return publisher.PublishGiveBenefit(payload)
+
+}
+
+func GetBenefitFromOvoNSQ(userID int) error {
+	fmt.Println("GetBenefitFromOvo....")
+	fmt.Println("UserID: ", userID)
 
 	resp, err := http.Get(fmt.Sprintf("http://localhost:10000/giveovo?user_id=%d", userID))
 	if err != nil {
---
+++ config/config.go
@@ -0,0 +1,8 @@
+package config
+
+const (
+	NSQLookupdURL = "127.0.0.1:4150"
+	Port          = "8080"
+	NSQTopic      = "GiveBenefitOvo"
+	NSQChannel    = "GiveBenefitOvo"
+)
